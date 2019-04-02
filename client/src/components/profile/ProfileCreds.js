2a12e573c0316d3e2543f541510351023b586f41
Deluxateer <deluxateer@gmail.com>
2019-04-02 12:57:35 -0500

    adds ProfileAbout, ProfileCred, ProfileGithub comp code"

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,10 +1,86 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Moment from "react-moment";
 
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+
+    const expItems = experience.map(exp => (
+      <li key={exp._id} className="list-group-item">
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          {exp.to === null ? (
+            "Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Position:</strong> {exp.title}
+        </p>
+        <p>
+          {exp.location === "" ? null : (
+            <Fragment>
+              <strong>Location: </strong> {exp.location}
+            </Fragment>
+          )}
+        </p>
+        <p>
+          {exp.description === "" ? null : (
+            <Fragment>
+              <strong>Description: </strong> {exp.description}
+            </Fragment>
+          )}
+        </p>
+      </li>
+    ));
+
+    const eduItems = education.map(edu => (
+      <li key={edu._id} className="list-group-item">
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          {edu.to === null ? (
+            "Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{edu.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Degree:</strong> {edu.degree}
+        </p>
+        <p>
+          <strong>Field Of Study:</strong> {edu.fieldofstudy}
+        </p>
+        <p>
+          {edu.description === "" ? null : (
+            <Fragment>
+              <strong>Description: </strong> {edu.description}
+            </Fragment>
+          )}
+        </p>
+      </li>
+    ));
+
     return (
-      <div>
-        <h1>Todo: Profile Creds</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          {expItems.length > 0 ? (
+            <ul className="list-group">{expItems}</ul>
+          ) : (
+            <p className="text-center">No Experience Listed</p>
+          )}
+        </div>
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          {expItems.length > 0 ? (
+            <ul className="list-group">{eduItems}</ul>
+          ) : (
+            <p className="text-center">No Education Listed</p>
+          )}
+        </div>
       </div>
     );
   }
